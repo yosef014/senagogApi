4149a40ee5b166c071e46b6b9e52b03b9ff29844
ariel levi <ariel22411@gmail.com>
2023-04-11 14:48:42 +0300

    dfgdfg

--- src/migrations/1681151831817-create_user_admin.ts
+++ src/migrations/1681151831817-create_user_admin.ts
@@ -1,28 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 const bcrypt = require('bcrypt');
-import { User } from "../../src/resources/users/entities/user.entity"
-
-
+import { User } from '../../src/resources/users/entities/user.entity';
 
 export class createUserAdmin1681151831817 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const bcryptPassword = await bcrypt.hash('123456', 10);
+    console.log('bcryptPassword', bcryptPassword);
+    const savedUser = await User.create({
+      fist_name: 'admin',
+      last_name: 'admin',
+      mobile: '0528302775',
+      email: 'yosef014@gmail.com',
+      senagog_id: 1,
+      permission_type: 'ADMIN',
+      password: bcryptPassword,
+      created_at: new Date(),
+      user_name: 'admin',
+    }).save();
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        const bcryptPassword = await bcrypt.hash('123456', 10);
-        console.log('bcryptPassword', bcryptPassword);
-        const savedUser = await User.create({
-          fist_name: 'admin',
-          last_name: 'admin',
-          mobile: '0528302775',
-          email: 'yosef014@gmail.com',
-          senagog_id: 1,
-          permission_type: 'ADMIN',
-          password: bcryptPassword,
-          created_at: new Date(),
-          user_name: 'admin'
-        }).save()
-     }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
