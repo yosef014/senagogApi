8b838d0f5b0b7f2409ea15355b8f9785132b33fc
Yosef Plishtiiv <yosef@wittix.com>
2023-04-16 09:18:55 +0300

    dsa

--- src/migrations/1681151831817-create_user_admin.ts
+++ src/migrations/1681151831817-create_user_admin.ts
@@ -5,24 +5,25 @@ import {Senagog} from "../resources/senagogs/entities/senagog.entity";
 
 export class createUserAdmin1681151831817 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const senagog = await Senagog.create({
-      name: 'חבד גני איילון',
-      description: 'בית כנסת חבד בגני איילון'
-    })?.save()
-    const bcryptPassword = await bcrypt.hash('123456', 10);
-    console.log('bcryptPassword', bcryptPassword);
-    const savedUser = await User.create({
-      fist_name: 'admin',
-      last_name: 'admin',
-      mobile: '0528302775',
-      email: 'yosef014@gmail.com',
-      senagog_id: senagog.id,
-      permission_type: 'ADMIN',
-      password: bcryptPassword,
-      created_at: new Date(),
-      updated_at: new Date(),
-      user_name: 'admin',
-    }).save();
+    console.log('asdasdasdasdasdsdasds')
+    // const senagog = await Senagog.create({
+    //   name: 'חבד גני איילון',
+    //   description: 'בית כנסת חבד בגני איילון'
+    // })?.save()
+    // const bcryptPassword = await bcrypt.hash('123456', 10);
+    // console.log('bcryptPassword', bcryptPassword);
+    // const savedUser = await User.create({
+    //   fist_name: 'admin',
+    //   last_name: 'admin',
+    //   mobile: '0528302775',
+    //   email: 'yosef014@gmail.com',
+    //   senagog_id: senagog.id,
+    //   permission_type: 'ADMIN',
+    //   password: bcryptPassword,
+    //   created_at: new Date(),
+    //   updated_at: new Date(),
+    //   user_name: 'admin',
+    // }).save();
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
