206fe729c1cb1aae165252dfabdb442bf5f6628d
yosef plishtiiv <97189433+yosef014@users.noreply.github.com>
2023-04-16 08:02:29 +0300

    Add files via upload

---
+++ migrations/1681151831817-create_user_admin.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import {Senagog} from "src/resources/senagogs/entities/senagog.entity";
+import {User} from "src/resources/users/entities/user.entity";
+const bcrypt = require('bcrypt');
+
+
+export class createUserAdmin1681151831817 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+
+      console.log('beforeee')
+      // const senagog = await Senagog.create({
+      //     name: 'חבד גני איילון',
+      //     description: 'בית כנסת חבד בגני איילון'
+      // }).save()
+      // console.log('senagog afger', senagog)
+    const bcryptPassword = await bcrypt.hash('123456', 10);
+    // console.log('bcryptPassword', bcryptPassword);
+    // const savedUser = await User.create({
+    //   fist_name: 'admin',
+    //   last_name: 'admin',
+    //   mobile: '0528302775',
+    //   email: 'yosef014@gmail.com',
+    //   senagog_id: senagog.id,
+    //   permission_type: 'ADMIN',
+    //   password: bcryptPassword,
+    //   user_name: 'admin',
+    // }).save();
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
